ed8f2a77ff782dd0c6bb9709dbdc7ce6315aee0c
Madush99 <madusankagajadeera99@gmail.com>
2021-08-17 02:56:42 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,12 @@ import LoginScreen from './screens/LoginScreen/loginScreen'
 import SignupScreen from './screens/SignupScreen/singupScreen'
 import RoomsScreen from './screens/RoomScreen/roomsScreen';
 import LandingScreen from './screens/LandingScreen/landingScreen';
-<<<<<<< HEAD
 import ViewRoomScreen from './screens/ViewRoomScreen/viewRoomScreen';
-=======
 import UserProfile from './screens/UserProfileScreen/userProfileScreen'
 
 
 
 
->>>>>>> e158c34497d68e0ee34fff6654a402de26357c75
 
 const App = () => {
   return (
@@ -27,12 +24,9 @@ const App = () => {
         <Route path="/rooms" component={RoomsScreen} />
         <Route path="/login" component={LoginScreen} />
         <Route path="/signup" component={SignupScreen} />
-<<<<<<< HEAD
         <Route path="/room/:id" component={ViewRoomScreen} />
 
-=======
         <Route path="/profile" component={UserProfile} />
->>>>>>> e158c34497d68e0ee34fff6654a402de26357c75
       </div>
     </Router>
   );
