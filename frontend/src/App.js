eccff631c3121795b3ffa98342762e72a05e50a0
Madush99 <madusankagajadeera99@gmail.com>
2021-08-16 11:32:08 +0530

    login and signup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,19 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 
+import Navbar from './components/Navbar/navbar';
+import LoginScreen from './screens/LoginScreen/loginScreen'
+import SignupScreen from './screens/SignupScreen/singupScreen'
 import RoomsScreen from './screens/RoomScreen/roomsScreen';
 
 const App = () => {
   return (
     <Router>
       <div>
+        <Navbar />
         <Route path="/rooms" component={RoomsScreen} exact />
-
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/signup" component={SignupScreen} />
 
       </div>
     </Router>
---
+++ frontend/src/screens/LoginScreen/loginScreen.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './login.css'
+
+const LoginScreen = () => {
+
+      return (
+            <>
+                  <div className="maincontainer">
+                        <div class="container-fluid">
+                              <div class="row no-gutter">
+
+                                    <div class="col-md-6 d-none d-md-flex bg-image"></div>
+
+                                    <div class="col-md-6 bg-light">
+                                          <div class="login d-flex align-items-center py-5">
+
+                                                <div class="container">
+                                                      <div class="row">
+                                                            <div class="col-lg-10 col-xl-7 mx-auto">
+                                                                  <h3 class="display-4">Split page!</h3>
+                                                                  <p class="text-muted mb-4">Create a login split page using Bootstrap 4.</p>
+                                                                  <form>
+                                                                        <div class="form-group mb-3">
+                                                                              <input id="inputEmail" type="email" placeholder="Email address" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" />
+                                                                        </div>
+                                                                        <div class="form-group mb-3">
+                                                                              <input id="inputPassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
+                                                                        </div>
+                                                                        <div class="custom-control custom-checkbox mb-3">
+                                                                              <input id="customCheck1" type="checkbox" checked class="custom-control-input" />
+                                                                              <label for="customCheck1" class="custom-control-label">Remember password</label>
+                                                                        </div>
+                                                                        <button type="submit" class="btn btn-outline-warning btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
+                                                                        <div class="text-center d-flex justify-content-between mt-4"><p>Code by <a href="#" class="font-italic text-muted">
+                                                                              <u>Jassa</u></a></p></div>
+                                                                  </form>
+                                                            </div>
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </>
+      )
+}
+
+export default LoginScreen
