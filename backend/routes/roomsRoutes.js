389cd24a580d2341cf512c4a94c06a7d016d7d4e
Madush99 <madusankagajadeera99@gmail.com>
2021-08-16 12:45:19 +0530

    getroomsbyid

--- backend/routes/roomsRoutes.js
+++ backend/routes/roomsRoutes.js
@@ -2,8 +2,9 @@ import express from 'express'
 
 const router = express.Router()
 
-import { getAllRooms } from '../controllers/roomsController.js'
+import { getAllRooms, getRoomsById } from '../controllers/roomsController.js'
 
 router.get('/allrooms', getAllRooms)
+router.get('/:id', getRoomsById)
 
 export default router
