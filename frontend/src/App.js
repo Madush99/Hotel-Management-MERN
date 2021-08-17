65a7c63fb46315c9cd860abf3f372b86004eba82
Madush99 <madusankagajadeera99@gmail.com>
2021-08-17 20:32:19 +0530

    merge changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,13 +28,8 @@ const App = () => {
         <Route path="/login" component={LoginScreen} />
         <Route path="/signup" component={SignupScreen} />
         <Route path="/room/:id" component={ViewRoomScreen} />
-<<<<<<< HEAD
         <Route path="/roombook/:roomid/:fromdate/:todate" component={BookingScreen} />
-=======
         <Route path="/restaurants" component={Restaurents} />
-
-        <Route path="/roombook/:roomid" component={BookingScreen} />
->>>>>>> 3c68e9584ce16bf0935eb4283208a0b37758267e
         <Route path="/profile" component={UserProfile} />
 
       </div>
