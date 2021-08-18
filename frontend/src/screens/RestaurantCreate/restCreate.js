759fa2e675283ddacef09ee3dff775f3cd1712e6
Agasthi98 <sankalana98@gmail.com>
2021-08-18 23:49:44 +0530

    dropdown okay

--- frontend/src/screens/RestaurantCreate/restCreate.js
+++ frontend/src/screens/RestaurantCreate/restCreate.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Select, Dropdown } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
@@ -14,171 +14,184 @@ import { createRest } from '../../actions/restaurantsActions'
 
 
 const RestEditScreen = ({ match, history }) => {
-   
-
-    const [name, setName] = useState('')
-    const [type, setType] = useState('')
-    const [tables, setTables] = useState(0)
-    const [phoneNo, setPhoneNo] = useState('')
-    const [email, setEmail] = useState('')
-    const [location, setLocation] = useState('')
-    const [image, setImage] = useState('')
-    const [description, setDescription] = useState('')
-    const [uploading, setUploading] = useState(false)
 
 
-    const dispatch = useDispatch()
+      const [name, setName] = useState('')
+      const [type, setType] = useState('')
+      const [tables, setTables] = useState(0)
+      const [phoneNo, setPhoneNo] = useState('')
+      const [email, setEmail] = useState('')
+      const [location, setLocation] = useState('')
+      const [image, setImage] = useState('')
+      const [description, setDescription] = useState('')
+      const [uploading, setUploading] = useState(false)
 
 
-    const createRestaurant = useSelector((state) => state.createRestaurant)
-    const { loading, error, restaurants} = createRestaurant
+      const dispatch = useDispatch()
 
-    useEffect(() => {
-          if (restaurants) {
-                history.push(Redirect)
-          }
-    }, [dispatch, restaurants, Redirect])
 
-    const submitHandler = (e) => {
-      e.preventDefault()
-            dispatch(createRest(name,type,tables,phoneNo,email,location,image,description))
-}
+      const createRestaurant = useSelector((state) => state.createRestaurant)
+      const { loading, error, restaurants } = createRestaurant
 
-    const uploadFileHandler = async (e) => {
-      const file = e.target.files[0]
-      const formData = new FormData()
-      formData.append('image', file)
-      setUploading(true)
+      useEffect(() => {
+            if (restaurants) {
+                  history.push(Redirect)
+            }
+      }, [dispatch, restaurants, Redirect])
 
-          try {
-                const config = {
-                      headers: {
-                            'Content-Type': 'multipart/form-data',
-                      },
-                }
+      const submitHandler = (e) => {
+            e.preventDefault()
+            dispatch(createRest(name, type, tables, phoneNo, email, location, image, description))
+      }
 
-                const { data } = await axios.post('/api/uploads/image', formData, config)
+    
 
-                setImage(data)
-                setUploading(false)
-          } catch (error) {
-                console.error(error)
-                setUploading(false)
-          }
-    }
+      const uploadFileHandler = async (e) => {
+            const file = e.target.files[0]
+            const formData = new FormData()
+            formData.append('image', file)
+            setUploading(true)
 
-    return (
-          <>
-                <Link to='/admin/productlist' className='btn btn-light my-3'>
-                      Go Back
-    </Link>
-                <FormContainer>
-                      <h1>ADD RESTAURANTS</h1>
-                      {loading && <Loader />}
-                      {error && <Message variant='danger'>{error} </Message>}
-                      {loading ? (
-                            <Loader />
-                      ) : error ? (
-                            <Message variant='danger'>{error}</Message>
-                      ) : (
-                            <Form onSubmit={submitHandler}>
-                                  <Form.Group controlId='name'>
-                                        <Form.Label>Name</Form.Label>
-                                        <Form.Control
-                                              type='name'
-                                              placeholder='Enter name'
-                                              value={name}
-                                              onChange={(e) => setName(e.target.value)}
-                                        ></Form.Control>
-                                  </Form.Group>
+            try {
+                  const config = {
+                        headers: {
+                              'Content-Type': 'multipart/form-data',
+                        },
+                  }
 
-                                  <Form.Group controlId='type'>
+                  const { data } = await axios.post('/api/uploads/image', formData, config)
+
+                  setImage(data)
+                  setUploading(false)
+            } catch (error) {
+                  console.error(error)
+                  setUploading(false)
+            }
+      }
+
+      return (
+            <>
+                  <Link to='/admin/productlist' className='btn btn-light my-3'>
+                        Go Back
+                  </Link>
+                  <FormContainer>
+                        <h1 style={{ textAlign: "center" }}>ADD RESTAURANTS</h1>
+                        {loading && <Loader />}
+                        {error && <Message variant='danger'>{error} </Message>}
+                        {loading ? (
+                              <Loader />
+                        ) : error ? (
+                              <Message variant='danger'>{error}</Message>
+                        ) : (
+                              <Form onSubmit={submitHandler}>
+                                    <Form.Group controlId='name'>
+                                          <Form.Label>Name</Form.Label>
+                                          <Form.Control
+                                                type='name'
+                                                placeholder='Enter name'
+                                                value={name}
+                                                onChange={(e) => setName(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='type'>
                                         <Form.Label>Type</Form.Label>
-                                        <Form.Control
-                                              type='text'
-                                              placeholder='Enter type'
-                                              value={type}
-                                              onChange={(e) => setType(e.target.value)}
-                                        ></Form.Control>
-                                  </Form.Group>
+                                          <Form value={type} onChange={(e) => setType(e.target.value)}>
+                                                <select style={{ width: "100%" }}>
+                                                      <option  value= "restaurant">Restaurant </option>
+                                                      <option  value= "restaurant">Bar </option>
+                                               </select>
+                                          </Form>
+                                       
+                                  </Form.Group> 
 
-                                  <Form.Group controlId='number'>
-                                        <Form.Label>Tables</Form.Label>
-                                        <Form.Control
-                                              type='number'
-                                              placeholder='Enter tables'
-                                              value={tables}
-                                              onChange={(e) => setTables(e.target.value)}
-                                        ></Form.Control>
-                                  </Form.Group>
 
-                                  <Form.Group controlId='price'>
-                                        <Form.Label>Phone Number</Form.Label>
-                                        <Form.Control
-                                              type='text'
-                                              placeholder='Enter Phone No'
-                                              value={phoneNo}
-                                              onChange={(e) => setPhoneNo(e.target.value)}
-                                        ></Form.Control>
-                                  </Form.Group>
+                                    {/* <Form.Group controlId='type'>
+                                    <Form.Label>Type</Form.Label>
+                                    <Form.Dropdown  onChange={ (e) => setType(e.target.value)}>
+                                          <Dropdown value="Restaurant">Restaurant</Dropdown>
+                                          <Dropdown value="Bar">Bar</Dropdown>
+                                    </Form.Dropdown>
+                                    </Form.Group> */}
 
-                                  <Form.Group controlId='email'>
-                                        <Form.Label>Email</Form.Label>
-                                        <Form.Control
-                                              type='email'
-                                              placeholder='Enter Email'
-                                              value={email}
-                                              onChange={(e) => setEmail(e.target.value)}
-                                        ></Form.Control>
-                                  </Form.Group>
 
-                                  <Form.Group controlId='price'>
-                                        <Form.Label>Location</Form.Label>
-                                        <Form.Control
-                                              type='text'
-                                              placeholder='Enter Location'
-                                              value={location}
-                                              onChange={(e) => setLocation(e.target.value)}
-                                        ></Form.Control>
-                                  </Form.Group>
+                                    <Form.Group controlId='number'>
+                                          <Form.Label>Tables</Form.Label>
+                                          <Form.Control
+                                                type='number'
+                                                placeholder='Enter tables'
+                                                value={tables}
+                                                onChange={(e) => setTables(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
 
-                                  <Form.Group controlId='description'>
-                                        <Form.Label>Descrition</Form.Label>
-                                        <Form.Control
-                                              type='text'
-                                              placeholder='Enter price'
-                                              value={description}
-                                              onChange={(e) => setDescription(e.target.value)}
-                                        ></Form.Control>
-                                  </Form.Group>
+                                    <Form.Group controlId='price'>
+                                          <Form.Label>Phone Number</Form.Label>
+                                          <Form.Control
+                                                type='text'
+                                                placeholder='Enter Phone No'
+                                                value={phoneNo}
+                                                onChange={(e) => setPhoneNo(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
 
-                                  <Form.Group controlId='image'>
-                                        <Form.Label>Image</Form.Label>
-                                        <Form.Control
-                                              type='text'
-                                              placeholder='Enter Image url'
-                                              value={image}
-                                              onChange={(e) => setImage(e.target.value)}
-                                        ></Form.Control>
-                                        <Form.File
-                                              id='image-file'
-                                              label='Choose File'
-                                              custom
-                                              onChange={uploadFileHandler}
-                                        ></Form.File>
-                                        {uploading && <Loader />}
-                                  </Form.Group>
+                                    <Form.Group controlId='email'>
+                                          <Form.Label>Email</Form.Label>
+                                          <Form.Control
+                                                type='email'
+                                                placeholder='Enter Email'
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
 
-                                
+                                    <Form.Group controlId='price'>
+                                          <Form.Label>Location</Form.Label>
+                                          <Form.Control
+                                                type='text'
+                                                placeholder='Enter Location'
+                                                value={location}
+                                                onChange={(e) => setLocation(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
 
-                                  <Button type='submit' variant='primary'>
-                                        ADD 
-                                  </Button>
-                            </Form>
-                      )}
-                </FormContainer>
-          </>
-    )
+                                    <Form.Group controlId='description'>
+                                          <Form.Label>Descrition</Form.Label>
+                                          <Form.Control
+                                                type='text'
+                                                placeholder='Enter price'
+                                                value={description}
+                                                onChange={(e) => setDescription(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='image'>
+                                          <Form.Label>Image</Form.Label>
+                                          {/* <Form.Control
+                                                type='text'
+                                                placeholder='Enter Image url'
+                                                value={image}
+                                                onChange={(e) => setImage(e.target.value)}
+                                          ></Form.Control> */}
+                                          <Form.File
+                                                id='image-file'
+                                                label='Choose File'
+                                                custom
+                                                onChange={uploadFileHandler}
+                                          ></Form.File>
+                                          {uploading && <Loader />}
+                                    </Form.Group>
+
+
+
+                                    <Button type='submit' variant='primary' style={{ width: "100%", height: "35px" }} >
+                                         ADD 
+                                    </Button>
+                              </Form>
+                        )}
+                  </FormContainer>
+            </>
+      )
 }
 
 export default RestEditScreen
