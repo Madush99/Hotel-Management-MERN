ca9040e888837f1831174f8eb78c0caef3c5a477
Agasthi98 <sankalana98@gmail.com>
2021-08-18 13:36:51 +0530

    login fixed

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -25,10 +25,19 @@ const reducer = combineReducers({
 })
 
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null
+
+const initialState = {
+    userLogin: { userInfo: userInfoFromStorage }
+}
+
 const middleware = [thunk]
 
 const store = createStore(
       reducer,
+      initialState,
       composeWithDevTools(applyMiddleware(...middleware))
 )
 
