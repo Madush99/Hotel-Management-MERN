fd531c56a24e860b1f5444b8f224f91410c7de57
davebulner <davebulner3@gmail.com>
2021-08-20 15:45:08 +0530

    wed insert

--- backend/controllers/weddingController.js
+++ backend/controllers/weddingController.js
@@ -2,55 +2,56 @@ import asyncHandler from 'express-async-handler'
 import Wedding from '../models/weddingModel.js'
 
 
-const insertWedding = asyncHandler(async(req, res) => {
+// const insertWedding = asyncHandler(async(req, res) => {
 
 
-    const { wedHallName,
-        wedSeats,
-        wedDes,
-        wedImage
-    } = req.body
-
-   const wedding = await Wedding.create({
-    wedHallName,
-    wedSeats,
-    wedDes,
-    wedImage
-   })
-
-   if(wedding) {
-       res.status(201).json({
-        _id: wedding._id,
-        wedHallName: wedding.wedHallName,
-        wedSeats: wedding.wedSeats,
-        wedDes: wedding.wedDes,
-        wedImage: wedding.wedImage,
-       })
-   } else {
-       res.status(400)
-       throw new Error('Invalid wedding hall data')
-   }
-
-})
-
-// const insertWedding = asyncHandler(async (req, res) => {
-
-//     const { wedHallName, wedSeats, wedDes, wedimg1, wedimg2, wedimg3 } = req.body
-
-//     const newwedding = new Wedding({
-//         wedHallName,
+//     const { wedHallName,
 //         wedSeats,
-//         wedImage: [wedimg1, wedimg2, wedimg3],
 //         wedDes,
-        
-//     })
-//     try {
-//           await newwedding.save()
-//           res.send('New Wedding Added Successfully')
-//     } catch (error) {
-//           return res.status(400).json({ error });
-//     }
+//         wedImage
+//     } = req.body
+
+//    const wedding = await Wedding.create({
+//     wedHallName,
+//     wedSeats,
+//     wedDes,
+//     wedImage
+//    })
+
+//    if(wedding) {
+//        res.status(201).json({
+//         _id: wedding._id,
+//         wedHallName: wedding.wedHallName,
+//         wedSeats: wedding.wedSeats,
+//         wedDes: wedding.wedDes,
+//         wedImage: wedding.wedImage,
+//        })
+//    } else {
+//        res.status(400)
+//        throw new Error('Invalid wedding hall data')
+//    }
+
 // })
+
+const insertWedding = asyncHandler(async (req, res) => {
+
+    const { wedHallName, wedSeats, wedDes, wedimg1, wedimg2, wedimg3 } = req.body
+
+    const newwedding = new Wedding({
+        wedHallName,
+        wedSeats,
+        wedImages: [wedimg1, wedimg2, wedimg3],
+        wedDes,
+        
+    })
+    
+    try {
+          await newwedding.save()
+          res.send('New Wedding Added Successfully')
+    } catch (error) {
+          return res.status(400).json({ error });
+    }
+})
 
 const getAllWedDetails = asyncHandler(async (req, res) => {
     const wedding = await Wedding.find({})
