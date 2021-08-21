af7cd589ef5312d36286567f966fb2cbef023f36
Madush99 <madusankagajadeera99@gmail.com>
2021-08-21 14:04:17 +0530

    food detail page css

--- frontend/src/screens/FoodDetailScreen/foodDetailScreen.js
+++ frontend/src/screens/FoodDetailScreen/foodDetailScreen.js
@@ -8,6 +8,7 @@ import 'aos/dist/aos.css'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
 import LinkContainer from 'react-router-bootstrap/lib/LinkContainer'
+import '../FoodDetailScreen/foodDetail.css'
 
 Aos.init()
 Aos.refresh()
@@ -41,87 +42,114 @@ const FoodDetail = ({ match, history }) => {
 
     return (
         <>
-            <Link className='btn btn-light my-3' to='/'>
-                Go Back
-            </Link>
             {loading ? (
                 <Loader />
             ) : error ? (
                 <Message variant='danger'>{error}</Message>
             ) : (
                 <>
+                    <div className="container">
+                        <div className='op' data-aos="zoom-in">
+                            <Row>
 
-                    <Row>
-                        <Col md={6}>
-                            <Image src={foods.image} alt={foods.name} fluid />
-                        </Col>
+                                <Col md={7}>
+                                    <Image src={foods.image} alt={foods.name} fluid />
+                                </Col>
 
-                        <Col md={3}>
-                            <Card>
-                                <ListGroup variant='flush'>
-                                    <ListGroup.Item>
-                                        <Row>
-                                            <Col>Price:</Col>
-                                            <Col>
-                                                <strong>Rs.{foods.price}</strong>
-                                            </Col>
-                                        </Row>
-                                    </ListGroup.Item>
+                                <Col md={5} className="rt">
+                                    <h4>ADD TO CART</h4>
+                                    <Card >
+                                        <ListGroup variant='flush'>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Category:</Col>
+                                                    <Col>
+                                                        <strong> {foods.category}</strong>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Price:</Col>
+                                                    <Col>
+                                                        <strong>LKR {foods.price}.00</strong>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
 
-                                    <ListGroup.Item>
-                                        <Row>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Qty</Col>
+                                                    <Col>
+                                                        <Form.Control
+                                                            type='number'
+                                                            placeholder='Enter name'
+                                                            value={qty}
+                                                            onChange={(e) => setQty(e.target.value)}
+                                                        >
+                                                        </Form.Control>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
 
 
-                                        </Row>
-                                    </ListGroup.Item>
-
-
-                                    <ListGroup.Item>
-                                        <Row>
-                                            <Col>Qty</Col>
-                                            <Col>
-                                                <Form.Control
-                                                    type='number'
-                                                    placeholder='Enter name'
-                                                    value={qty}
-                                                    onChange={(e) => setQty(e.target.value)}
+                                            <ListGroup.Item>
+                                                <Button
+                                                    onClick={addToCartHandler}
+                                                    className='btn-block '
+                                                    type='button'
+                                                    variant='warning'
                                                 >
-                                                </Form.Control>
-                                            </Col>
-                                        </Row>
-                                    </ListGroup.Item>
+                                                    Add To Cart
+                                                </Button>
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Card>
+                                    <hr></hr>
+                                    <b><p>Phone
+                                        (+94) 11 788 8288</p></b>
+                                    <b><p>Email
+                                        restaurants.slcb@juice.com</p></b>
 
+                                </Col>
+                            </Row>
 
-                                    <ListGroup.Item>
-                                        <Button
-                                            onClick={addToCartHandler}
-                                            className='btn-block nm'
-                                            type='button'
+                            <Row>
+                                <Col md={7}>
+                                    {/* <ListGroup variant='flush'>
+                                        <div className='fg'>
+                                            <ListGroup.Item>
+                                                <h3>{foods.name}</h3>
+                                            </ListGroup.Item>
+                                        </div>
+                                        <ListGroup.Item>Price: LKR {foods.price}</ListGroup.Item>
+                                        <ListGroup.Item>
+                                            Description: {foods.description}
+                                        </ListGroup.Item>
+                                    </ListGroup> */}
 
-                                        >
-                                            Add To Cart
-                                        </Button>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={3}>
-                            <ListGroup variant='flush'>
-                                <ListGroup.Item>
-                                    <h3>{foods.name}</h3>
-                                </ListGroup.Item>
+                                    <br></br>
+                                    <div class="fg">
+                                        <div className="df">
+                                            <h6>Shangri-La Colombo</h6>
+                                            <h1> {foods.name}</h1>
+                                        </div>
+                                    </div>
+                                    <hr></hr>
 
-                                <ListGroup.Item>Price: RS.{foods.price}</ListGroup.Item>
-                                <ListGroup.Item>
-                                    Description: {foods.description}
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Col>
-                    </Row>
+                                    <h6>LKR {foods.price}.00</h6>
+                                    <h6>About the ItemS</h6>
+                                    <p>{foods.description}</p>
+                                </Col>
+                                <Col md={5} className="re">
+
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
                 </>
-            )}
+            )
+            }
         </>
     )
 }
