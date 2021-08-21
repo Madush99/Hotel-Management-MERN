8e13a3f1861ff459e8fee9894a9b1d919e572066
Madush99 <madusankagajadeera99@gmail.com>
2021-08-21 14:21:59 +0530

    cart css

--- frontend/src/screens/CartScreen/cartScreen.js
+++ frontend/src/screens/CartScreen/cartScreen.js
@@ -32,81 +32,85 @@ const CartScreen = ({ match, location, history }) => {
 
   return (
     <>
-      <Link to='/' className='btn btn-light'>Go Back</Link>
-      <Row>
-        <Col md={8}>
-          <h1>Shopping Cart</h1>
-          {cartItems.length === 0 ? (
-            <Message>
-              Your cart is empty <Link to='/'>Go Back</Link>
-            </Message>
-          ) : (
-            <ListGroup variant='flush'>
-              {cartItems.map((item) => (
-                <ListGroup.Item key={item.food}>
-                  <Row>
-                    <Col md={2}>
-                      <Image src={item.image} alt={item.name} fluid rounded />
-                    </Col>
-                    <Col md={3}>
-                      <Link className='link' to={`/product/${item.product}`}>{item.name}</Link>
-                    </Col>
-                    <Col md={2}>Rs.{item.price}</Col>
-                    <Col md={2}>
-                      <Form.Control
-                        type='number'
-                        value={item.qty}
-                        onChange={(e) =>
-                          dispatch(
-                            addToCart(item.food, Number(e.target.value))
-                          )
-                        }
-                      >
-                       
-                      </Form.Control>
-                    </Col>
-                    <Col md={2}>
-                      <Button
-                        type='button'
-                        variant='light'
-                        onClick={() => removeFromCartHandler(item.food)}
-                      >
-                        <i className='fas fa-trash'></i>
-                      </Button>
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          )}
-        </Col>
-        <Col md={4} className='lo'>
-          <Card>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <h2>
-                  Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
-                items
-              </h2>
-              Rs.
-              {cartItems
-                  .reduce((acc, item) => acc + item.qty * item.price, 0)
-                  .toFixed(2)}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  type='button'
-                  className='btn-block nm'
-                  disabled={cartItems.length === 0}
-                  onClick={checkoutHandler}
-                >
-                  Proceed To Checkout
-              </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+      <div className="container">
+        <div className='op' data-aos="zoom-in">
+          <Row>
+            <Col md={8}>
+              <h1>SHOOPING CART</h1>
+              {cartItems.length === 0 ? (
+                <Message>
+                  Your cart is empty <Link to='/'>Go Back</Link>
+                </Message>
+              ) : (
+                <ListGroup variant='flush'>
+                  {cartItems.map((item) => (
+                    <ListGroup.Item key={item.food}>
+                      <Row>
+                        <Col md={2}>
+                          <Image src={item.image} alt={item.name} fluid rounded />
+                        </Col>
+                        <Col md={3}>
+                          <Link className='link' to={`/product/${item.product}`}><h5>{item.name}</h5></Link>
+                        </Col>
+                        <Col md={2}><h5>LKR {item.price}</h5></Col>
+                        <Col md={2}>
+                          <Form.Control
+                            type='number'
+                            value={item.qty}
+                            onChange={(e) =>
+                              dispatch(
+                                addToCart(item.food, Number(e.target.value))
+                              )
+                            }
+                          >
+
+                          </Form.Control>
+                        </Col>
+                        <Col md={2}>
+                          <Button
+                            type='button'
+                            variant='light'
+                            onClick={() => removeFromCartHandler(item.food)}
+                          >
+                            <i className='fas fa-trash'></i>
+                          </Button>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </Col>
+            <Col md={4} className='lo'>
+              <Card>
+                <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                    <h2>
+                      Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                      items
+                    </h2>
+
+                    <h5> LKR {cartItems
+                      .reduce((acc, item) => acc + item.qty * item.price, 0)
+                      .toFixed(2)}</h5>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      type='button'
+                      className='btn-block'
+                      disabled={cartItems.length === 0}
+                      onClick={checkoutHandler}
+                      variant='warning'
+                    >
+                      Proceed To Checkout
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
     </>
   )
 }
