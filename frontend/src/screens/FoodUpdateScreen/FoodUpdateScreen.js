878e1760f40962d7ab153c6dc4fa58225a5c2977
ushiniavindika <ushavindika@gmail.com>
2021-09-20 00:29:18 +0530

    food update form created

--- frontend/src/screens/FoodUpdateScreen/FoodUpdateScreen.js
+++ frontend/src/screens/FoodUpdateScreen/FoodUpdateScreen.js
@@ -44,9 +44,105 @@ const FoodUpdateScreen = (match) => {
         e.preventDefault()
         //Update foods
     }
+    return (
+        <>
+             <FormContainer>
+                        <h1 style={{ textAlign: "center" }}>Update Foods</h1>
+                        {loading && <Loader />}
+                        {error && <Message variant='danger'>{error} </Message>}
+                        {loading ? (
+                              <Loader />
+                        ) : error ? (
+                              <Message variant='danger'>{error}</Message>
+                        ) : (
+                              <Form onSubmit={submitHandler}>
+                                    <Form.Group controlId='name'>
+                                          <Form.Label>Name</Form.Label>
+                                          <Form.Control
+                                                type='name'
+                                                placeholder='Enter name'
+                                                value={name}
+                                                onChange={(e) => setName(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='name'>
+                                          <Form.Label>Category</Form.Label>
+                                          <Form.Control
+                                                type='name'
+                                                placeholder='Enter name'
+                                                value={category}
+                                                onChange={(e) => setCategory(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
 
 
+                                    {/* <Form.Group controlId='type'>
+                                    <Form.Label>Type</Form.Label>
+                                    <Form.Dropdown  onChange={ (e) => setType(e.target.value)}>
+                                          <Dropdown value="Restaurant">Restaurant</Dropdown>
+                                          <Dropdown value="Bar">Bar</Dropdown>
+                                    </Form.Dropdown>
+                                    </Form.Group> */}
+
+
+                                    <Form.Group controlId='number'>
+                                          <Form.Label>Price</Form.Label>
+                                          <Form.Control
+                                                type='number'
+                                                placeholder='Enter tables'
+                                                value={price}
+                                                onChange={(e) => setPrice(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
+
+                                   
+
+                                  
+
+                                    
+
+                                    <Form.Group controlId='description'>
+                                          <Form.Label>Descrition</Form.Label>
+                                          <Form.Control
+                                                type='text'
+                                                placeholder='Enter price'
+                                                value={description}
+                                                onChange={(e) => setDescription(e.target.value)}
+                                          ></Form.Control>
+                                    </Form.Group>
+
+                                  
+
+                                    <Form.Group controlId='image'>
+                                          <Form.Label>Image</Form.Label>
+                                          <Form.Control
+                                                type='text'
+                                                placeholder='Enter Image url'
+                                                value={image}
+                                                onChange={(e) => setImage(e.target.value)}
+                                          ></Form.Control>
+                                          <Form.File
+                                                id='image-file'
+                                                label='Choose File'
+                                                custom
+                                                // onChange={uploadFileHandler}
+                                          ></Form.File>
+                                          {uploading && <Loader />}
+                                    </Form.Group>
+                                  
+
+
+                                    <Button type='submit' variant='primary' style={{ width: "100%", height: "35px" }} >
+                                          ADD
+                                    </Button>
+                              </Form>
+                        )}
+                  </FormContainer>
+        </>
+    )
 
 
 } 
+
 export default FoodUpdateScreen
