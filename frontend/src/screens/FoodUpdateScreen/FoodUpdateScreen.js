53419b6e16680a7d5d610967c735115919cf379b
ushiniavindika <ushavindika@gmail.com>
2021-09-20 00:25:45 +0530

    useEffect and submit handler

--- frontend/src/screens/FoodUpdateScreen/FoodUpdateScreen.js
+++ frontend/src/screens/FoodUpdateScreen/FoodUpdateScreen.js
@@ -14,7 +14,7 @@ import { getFoodDetails } from '../../actions/foodsAction'
 
 const FoodUpdateScreen = (match) => {
     const foodId =match.params.id
-    
+
     const [name, setName] = useState('')
     const [category, setCategory] = useState('')
     const [price, setPrice] = useState(0)
@@ -26,6 +26,26 @@ const FoodUpdateScreen = (match) => {
 
     const foodDetailsByid = useSelector((state) => state.foodDetailsByid)
     const { loading, error, foods } = foodDetailsByid
+
+    useEffect(() =>{
+        if(!foods.name || foods._id !== foodId){
+            dispatch(getFoodDetails(foodId))
+        }else{
+            setName(foods.name)
+            setCategory(foods.category)
+            setPrice(foods.price)
+            setDescription(foods.description)
+            setImage(foods.image)
+    
+        }
+    },[dispatch,foodId,foods])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        //Update foods
+    }
+
+
 
 
 } 
