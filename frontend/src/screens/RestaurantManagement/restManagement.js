ec667e0b480a233a9d36132af1145db41703c47e
Agasthi98 <sankalana98@gmail.com>
2021-09-19 16:18:09 +0530

    search element created

--- frontend/src/screens/RestaurantManagement/restManagement.js
+++ frontend/src/screens/RestaurantManagement/restManagement.js
@@ -29,11 +29,21 @@ const RestaurantgementScreen = () => {
             <Container>
                   <div className="ml-3">
                         <h2 className="text-center m-2" style={{ fontSize: "35px" }}>RESTAURANT MANAGEMENT</h2>
-                        <Row className='align-items-center'>
+                        <br />
+                        <Row className=''>
+
+                              <Col className='text-left'>
+                                    <form class="form-inline my-2 my-lg-0">
+                                          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                                          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+                                    </form>
+                              </Col>
+
                               <Col className='text-right'>
-                                    <Button primary={true} onClick={handleExportWithComponent}> Report</Button>
+                                    <Button primary={true} onClick={handleExportWithComponent}>Generate Report</Button>
                               </Col>
                         </Row>
+                        <br />
                         <Tabs defaultActiveKey="1">
                               <TabPane tab="RESTAURANT LIST" key="1">
                                     <div className="row">
