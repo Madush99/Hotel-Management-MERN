ead8cd71103e25b6291e1bf624f6afed74eef023
ushiniavindika <ushavindika@gmail.com>
2021-09-30 00:59:16 +0530

    my order action complete in food

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -12,6 +12,9 @@ import {
       ORDER_LIST_REQUEST,
       ORDER_LIST_SUCCESS,
       ORDER_LIST_FAIL,
+      ORDER_LIST_MY_REQUEST,
+      ORDER_LIST_MY_SUCCESS,
+      ORDER_LIST_MY_FAIL
 } from '../constants/orderConstants'
 
 
@@ -124,6 +127,7 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
 
 
 
+
 export const listOrders = () => async (dispatch, getState) => {
       try {
             dispatch({
@@ -149,6 +153,39 @@ export const listOrders = () => async (dispatch, getState) => {
       } catch (error) {
             dispatch({
                   type: ORDER_LIST_FAIL,
+                  payload:
+                        error.response && error.response.data.message
+                              ? error.response.data.message
+                              : error.message,
+            })
+      }
+}
+
+export const listMyOrders = () => async (dispatch, getState) => {
+      try {
+            dispatch({
+                  type: ORDER_LIST_MY_REQUEST,
+            })
+
+            const {
+                  userLogin: { userInfo },
+            } = getState()
+
+            const config = {
+                  headers: {
+                        Authorization: `Bearer ${userInfo.token}`,
+                  },
+            }
+
+            const { data } = await axios.get(`/api/orders/myorders`, config)
+
+            dispatch({
+                  type: ORDER_LIST_MY_SUCCESS,
+                  payload: data,
+            })
+      } catch (error) {
+            dispatch({
+                  type: ORDER_LIST_MY_FAIL,
                   payload:
                         error.response && error.response.data.message
                               ? error.response.data.message
