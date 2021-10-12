aac2bd970b799ce9edf7080338cae39499b68c8b
Madush99 <madusankagajadeera99@gmail.com>
2021-10-12 23:25:02 +0530

    error fixed

--- backend/controllers/bookingContoller.js
+++ backend/controllers/bookingContoller.js
@@ -3,145 +3,133 @@ import Bookings from '../models/bookingModel.js'
 import Rooms from '../models/roomModel.js'
 import { v4 as uuidv4 } from 'uuid'
 import moment from 'moment'
-import Stripe from 'stripe';
+import Stripe from 'stripe'
 //import { Router } from 'express'
 
-const stripe = new Stripe('sk_test_51JPWGjSI37Hyu4LSE4vX93j1azAOjpcgYNLvdh9avZuqoUElA7PO2w8xquKa0Z4yt8Ad1lEdRMH4HRxiicAW3Gyc00IVYrwUME');
-
+const stripe = new Stripe(
+  'sk_test_51JPWGjSI37Hyu4LSE4vX93j1azAOjpcgYNLvdh9avZuqoUElA7PO2w8xquKa0Z4yt8Ad1lEdRMH4HRxiicAW3Gyc00IVYrwUME'
+)
 
 const bookRoom = asyncHandler(async (req, res) => {
+  const { rooms, userid, fromdate, todate, totalDays, totalAmount, token } =
+    req.body
 
+  try {
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    })
 
-      const { rooms, userid, fromdate, todate, totalDays, totalAmount, token } = req.body
+    console.log(customer.id)
 
-      try {
-
-            const customer = await stripe.customers.create({
-                  email: token.email,
-                  source: token.id,
-            });
-
-            console.log(customer.id);
-
-
-            const payment = await stripe.charges.create(
-                  {
-                        amount: totalAmount * 100,
-                        currency: "inr",
-                        customer: customer.id,
-                        receipt_email: token.email,
-                  },
-                  {
-                        idempotencyKey: uuidv4(),
-                  }
-            );
-
-
-            if (payment) {
-                  try {
-                        const newbooking = new Bookings({
-
-                              rooms: rooms.name,
-                              roomid: rooms._id,
-                              userid,
-                              totalDays: totalDays,
-                              fromdate: moment(fromdate).format("DD-MM-YYYY"),
-                              todate: moment(todate).format("DD-MM-YYYY"),
-                              totalAmount: totalAmount,
-                              transactionId: "1234",
-                              status: 'booked'
-                        });
-
-                        const booking = await newbooking.save()
-
-                        const roomtemp = await Rooms.findOne({ _id: rooms._id })
-
-                        roomtemp.currentBookings.push({
-                              bookingid: booking._id,
-                              fromdate: moment(fromdate).format("DD_MM_YYYY"),
-                              todate: moment(todate).format("DD-MM-YYYY"),
-                              userid: userid,
-                              status: booking.status
-                        })
-
-
-                        await roomtemp.save()
-
-                        // (async (err, bookings) => {
-                        //       const oldroom = await Rooms.findOne({ _id: rooms._id });
-
-                        //       oldroom.currentBookings.push({
-                        //             bookings: bookings._id,
-                        //             fromdate: moment(fromdate).format("DD-MM-YYYY"),
-                        //             todate: moment(todate).format("DD-MM-YYYY"),
-                        //             userid: user._id,
-                        //             status: 'booked'
-                        //       });
-                        //       await oldroom.save();
-                        // });
-
-
-
-                        res.send("Room Booked Successfully");
-
-                  } catch (error) {
-                        console.log(error);
-                        return res.status(400).json({ message: error });
-                  }
-            } else {
-                  res.send("Payment failed");
-            }
-
-      } catch (error) {
-            return res.status(400).json({ message: "Something went wrong" + error });
+    const payment = await stripe.charges.create(
+      {
+        amount: totalAmount * 100,
+        currency: 'inr',
+        customer: customer.id,
+        receipt_email: token.email,
+      },
+      {
+        idempotencyKey: uuidv4(),
       }
+    )
 
+    if (payment) {
+      try {
+        const newbooking = new Bookings({
+          rooms: rooms.name,
+          roomid: rooms._id,
+          userid,
+          totalDays: totalDays,
+          fromdate: moment(fromdate).format('DD-MM-YYYY'),
+          todate: moment(todate).format('DD-MM-YYYY'),
+          totalAmount: totalAmount,
+          transactionId: '1234',
+          status: 'booked',
+        })
 
+        const booking = await newbooking.save()
+
+        const roomtemp = await Rooms.findOne({ _id: rooms._id })
+
+        roomtemp.currentBookings.push({
+          bookingid: booking._id,
+          fromdate: moment(fromdate).format('DD_MM_YYYY'),
+          todate: moment(todate).format('DD-MM-YYYY'),
+          userid: userid,
+          status: booking.status,
+        })
+
+        await roomtemp.save()
+
+        // (async (err, bookings) => {
+        //       const oldroom = await Rooms.findOne({ _id: rooms._id });
+
+        //       oldroom.currentBookings.push({
+        //             bookings: bookings._id,
+        //             fromdate: moment(fromdate).format("DD-MM-YYYY"),
+        //             todate: moment(todate).format("DD-MM-YYYY"),
+        //             userid: user._id,
+        //             status: 'booked'
+        //       });
+        //       await oldroom.save();
+        // });
+
+        res.send('Room Booked Successfully')
+      } catch (error) {
+        console.log(error)
+        return res.status(400).json({ message: error })
+      }
+    } else {
+      res.send('Payment failed')
+    }
+  } catch (error) {
+    return res.status(400).json({ message: 'Something went wrong' + error })
+  }
 })
 
-
 const getallbookings = asyncHandler(async (req, res) => {
-      try {
-            const bookings = await Bookings.find({});
-            res.send(bookings);
-      } catch (error) {
-            return res.status(400).json({ message: error });
-      }
+  try {
+    const bookings = await Bookings.find({})
+    res.send(bookings)
+  } catch (error) {
+    return res.status(400).json({ message: error })
+  }
 })
 
 const getUserBookings = asyncHandler(async (req, res) => {
-      const { userid } = req.body;
-      try {
-            const bookings = await Bookings.find({ userid: userid }).sort({ _id: -1 })
-            res.send(bookings)
-      } catch (error) {
-            return res.status(400).json({ message: "Error" })
-      }
+  const { userid } = req.body
+  try {
+    const bookings = await Bookings.find({ userid: userid }).sort({ _id: -1 })
+    res.send(bookings)
+  } catch (error) {
+    return res.status(400).json({ message: 'Error' })
+  }
 })
-
 
 const cancelBookings = asyncHandler(async (req, res) => {
-      const { bookingid, roomid } = req.body
+  const { bookingid, roomid } = req.body
 
-      try {
-            const bookingitem = await Bookings.findOne({ _id: bookingid })
-            bookingitem.status = 'cancelled'
-            await bookingitem.save();
-            const room = await Rooms.findOne({ _id: roomid })
-            const bookings = room.currentBookings
-            const temp = bookings.filter(booking => typeof booking.bookingid == "undefined" || booking.bookingid.toString() !== bookingid)
-            console.log(temp);
-            room.currentBookings = temp;
-            await room.save()
+  try {
+    const bookingitem = await Bookings.findOne({ _id: bookingid })
+    bookingitem.status = 'cancelled'
+    await bookingitem.save()
+    const room = await Rooms.findOne({ _id: roomid })
+    const bookings = room.currentBookings
+    const temp = bookings.filter(
+      (booking) =>
+        typeof booking.bookingid == 'undefined' ||
+        booking.bookingid.toString() !== bookingid
+    )
+    console.log(temp)
+    room.currentBookings = temp
+    await room.save()
 
-            res.send('Bookings cancelled sucessfully')
-      } catch (error) {
-            console.log(error);
-            return res.status(400).json({ message: "error" })
-      }
+    res.send('Bookings cancelled sucessfully')
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ message: 'error' })
+  }
 })
-
-
-
 
 export { bookRoom, getallbookings, getUserBookings, cancelBookings }
--- frontend/src/screens/UserRoomBookings/userRoomBookimgs.js
+++ frontend/src/screens/UserRoomBookings/userRoomBookimgs.js
@@ -1,86 +1,111 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Swal from "sweetalert2";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
-import { Tag, Divider } from 'antd';
-
+import { Tag, Divider } from 'antd'
 
 const user = JSON.parse(localStorage.getItem('userInfo'))
 
 const UserRoomBookimgs = () => {
+  const [mybookings, setmybookings] = useState([])
+  const [loading, setloading] = useState(false)
+  const [error, seterror] = useState(false)
+  // const [success, setsuccess] = useState(false);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    try {
+      setloading(true)
+      const data = await (
+        await axios.post('/api/booking/userBookings', {
+          userid: JSON.parse(localStorage.getItem('userInfo'))._id,
+        })
+      ).data
+      setmybookings(data)
+      setloading(false)
+    } catch (error) {
+      setloading(false)
+      seterror(true)
+    }
+  }, [])
 
-
-
-      const [mybookings, setmybookings] = useState([]);
-      const [loading, setloading] = useState(false);
-      const [error, seterror] = useState(false);
-      // const [success, setsuccess] = useState(false);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      useEffect(async () => {
-            try {
-                  setloading(true);
-                  const data = await (
-                        await axios.post("/api/booking/userBookings", {
-                              userid: JSON.parse(localStorage.getItem("userInfo"))._id,
-                        })
-                  ).data;
-                  setmybookings(data);
-                  setloading(false);
-            } catch (error) {
-                  setloading(false);
-                  seterror(true);
-            }
-      });
-
-      async function cancelBooking(bookingid, roomid) {
-
-
-            try {
-                  setloading(true);
-                  const result = await (await axios.post('/api/booking/cancelbookings', { bookingid, roomid })).data
-                  setloading(false);
-                  Swal.fire('Congrats', 'Your Room has cancelled succeessfully', 'success').then(result => {
-                        window.location.href = '/profile'
-                  })
-            } catch (error) {
-                  Swal.fire('Oops', 'Something went wrong', 'error').then(result => {
-                        window.location.href = '/profile'
-                  })
-                  setloading(false)
-            }
-
-      }
-      return (
-            <>
-                  <div>
-                        {loading ? (
-                              <Loader />
-                        ) : error ? (
-                              <Message />
-                        ) : (
-                              mybookings.map(booking => {
-                                    return <div className="row">
-                                          <div className="col-md-6 my-auto">
-                                                <div className='bs m-1 p-2'>
-                                                      <h1>{booking.room}</h1>
-                                                      <p>BookingId : {booking._id}</p>
-                                                      <p>TransactionId : {booking.transactionId}</p>
-                                                      <p><b>Check In : </b>{booking.fromdate}</p>
-                                                      <p><b>Check Out : </b>{booking.todate}</p>
-                                                      <p><b>Amount : </b> {booking.totalAmount}</p>
-                                                      <p><b>Status</b> : {booking.status === 'booked' ? (<Tag color="green">Confirmed</Tag>) : (<Tag color="red">Cancelled</Tag>)}</p>
-                                                      <div className='text-right'>
-                                                            {booking.status === 'booked' && (<button className='btn btn-outline-danger' onClick={() => cancelBooking(booking._id, booking.roomid)}>Cancel Booking</button>)}
-                                                      </div>
-                                                </div>
-                                          </div>
-                                    </div>
-                              })
-                        )}
+  async function cancelBooking(bookingid, roomid) {
+    try {
+      setloading(true)
+      const result = await (
+        await axios.post('/api/booking/cancelbookings', { bookingid, roomid })
+      ).data
+      setloading(false)
+      Swal.fire(
+        'Congrats',
+        'Your Room has cancelled succeessfully',
+        'success'
+      ).then((result) => {
+        window.location.href = '/profile'
+      })
+    } catch (error) {
+      Swal.fire('Oops', 'Something went wrong', 'error').then((result) => {
+        window.location.href = '/profile'
+      })
+      setloading(false)
+    }
+  }
+  return (
+    <>
+      <div>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message />
+        ) : (
+          mybookings.map((booking) => {
+            return (
+              <div className='row'>
+                <div className='col-md-6 my-auto'>
+                  <div className='bs m-1 p-2'>
+                    <h1>{booking.room}</h1>
+                    <p>BookingId : {booking._id}</p>
+                    <p>TransactionId : {booking.transactionId}</p>
+                    <p>
+                      <b>Check In : </b>
+                      {booking.fromdate}
+                    </p>
+                    <p>
+                      <b>Check Out : </b>
+                      {booking.todate}
+                    </p>
+                    <p>
+                      <b>Amount : </b> {booking.totalAmount}
+                    </p>
+                    <p>
+                      <b>Status</b> :{' '}
+                      {booking.status === 'booked' ? (
+                        <Tag color='green'>Confirmed</Tag>
+                      ) : (
+                        <Tag color='red'>Cancelled</Tag>
+                      )}
+                    </p>
+                    <div className='text-right'>
+                      {booking.status === 'booked' && (
+                        <button
+                          className='btn btn-outline-danger'
+                          onClick={() =>
+                            cancelBooking(booking._id, booking.roomid)
+                          }
+                        >
+                          Cancel Booking
+                        </button>
+                      )}
+                    </div>
                   </div>
-            </>
-      )
+                </div>
+              </div>
+            )
+          })
+        )}
+      </div>
+    </>
+  )
 }
 
 export default UserRoomBookimgs
