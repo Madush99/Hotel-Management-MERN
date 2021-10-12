354778fa3e4745e3cbc14b2b6f510aba9e4f0f50
Madush99 <madusankagajadeera99@gmail.com>
2021-10-13 00:26:10 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ import connectDB from './config/db.js'
 import cors from 'cors'
 import path from 'path'
 
-
-
 //routes
 import roomRoutes from './routes/roomsRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -16,13 +14,12 @@ import weddingRoutes from './routes/weddingRoutes.js'
 import conferenceRoutes from './routes/conferenceRoutes.js'
 import conInsideRoutes from './routes/conInsideRoute.js'
 import uploadRoutes from './routes/uploadRoutes.js'
- 
+
 import bookingRoutes from './routes/bookingRoutes.js'
 
 import foodRoutes from './routes/foodRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import tableBookingRoutes from './routes/tableBookingRoutes.js'
-
 
 dotenv.config()
 
@@ -31,19 +28,12 @@ connectDB()
 
 const app = express()
 
-
-
-
 if (process.env.NODE_ENV === 'development') {
-      app.use(morgan('dev'))
+  app.use(morgan('dev'))
 }
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/', (req, res) => {
-      res.send('API is running...')
-})
 
 //calling routes
 app.use('/api/rooms', roomRoutes)
@@ -61,11 +51,29 @@ app.use('/api/booking', bookingRoutes)
 app.use('/api/food', foodRoutes)
 app.use('/api/tableBooking', tableBookingRoutes)
 
-
 //paypal id connect
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
-//create port 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
+
+//create port
 const PORT = process.env.PORT || 6500
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold
+  )
+)
